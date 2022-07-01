791b14981b4f194becdfb1908915d49f5f98bf94
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-07-01 16:59:14 -0400

    Created a basic weather component and css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
-
+import Weather from "./Weather";
+import './Weather.css';
 export default function App() {
   return (
     <div className="App">
       <h1> React Weather App</h1>
+      <Weather />
       <footer>
         This page was coded by Andrea Simmons and is{" "}
-        <a href="https://github.com/Ndrea3/react-weather-app" rel="nonreferrer" target="_blank">open-sourced.</a>
+        <a
+          href="https://github.com/Ndrea3/react-weather-app"
+          target="_blank"
+          rel="nonreferrer"
+        >
+          open-sourced in Github.
+        </a>
       </footer>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function weather(){
+    return(
+        <div className="weather"> The current weather is....</div>
+    );
+}
