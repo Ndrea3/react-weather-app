7ea7c9e9b07d37fc0b4b6f67c6119d1a3ea9be3c
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-07-01 17:02:37 -0400

    minor update to the app.js - footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
-import './Weather.css';
+import "./Weather.css";
+
 export default function App() {
   return (
     <div className="App">
-      <h1> React Weather App</h1>
-      <Weather />
-      <footer>
-        This page was coded by Andrea Simmons and is{" "}
-        <a
-          href="https://github.com/Ndrea3/react-weather-app"
-          target="_blank"
-          rel="nonreferrer"
-        >
-          open-sourced in Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Andrea Simmons and is
+          <a
+            href="https://github.com/Ndrea3/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {" "}
+            open-sourced in Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
