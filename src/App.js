aa87d151f4eb8f5b399ffff3eaf0ee8a631cafa0
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-07-01 11:15:45 -0400

    added the Github  repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Andrea Simmons and is
+        <a href="https://github.com/Ndrea3/react-weather-app" target="_blank">
+          {" "}
+          open-sourced in Github
+        </a>
+      </footer>
+    </div>
+  );
 }
