2a40347fd35ddf793030c9aeb0df014235909f61
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-07-01 16:14:24 -0400

    Revert "Adding the background image in App.js"
    This reverts commit 06f0870c8b691bfb56616c871b988a8055318436.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import "./App.css";
 import Weather from "./Weather";
 import "./Weather.css";
-import background from "./Images/coffee-and-donuts-pop-color-background.png";
-class App extends Component {
-  render() {
-    const myStyle = {
-      backgroundImage: `url(${background})`,
-      height: "100vh",
-      marginTop: "-70px",
-      fontSize: "50px",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-    };
 
-    export default function App() {
-      return (
-        <div className="App" style={myStyle}>
-          <div className="container">
-            <h1>Weather App</h1>
-            <Weather />
-            <footer>
-              This project was coded by Andrea Simmons and is
-              <a
-                href="https://github.com/Ndrea3/react-weather-app"
-                rel="noreferrer"
-                target="_blank"
-              >
-                {" "}
-                open-sourced in Github
-              </a>
-            </footer>
-          </div>
-        </div>
-      );
-    }
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Andrea Simmons and is
+          <a
+            href="https://github.com/Ndrea3/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {" "}
+            open-sourced in Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
